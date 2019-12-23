3143736755baa8a6c45a468b859ae062bb0a73e0
Kristopher Roy <tankc_g9eohxn@L5CG925174Q.btl.bellind.net>
2019-12-23 10:05:08 -0800

    Add project files.

---
+++ New-ADUser.ps1
@@ -0,0 +1,14 @@
+﻿#
+# New-ADUser.ps1
+#
+
+<# 
+	Created by Kristopher Roy
+	Created on 20Dec2019
+	Purpose - Created to coexist with a web front end to expidite the process of creating user accounts
+#>
+
+Import-Module activedirectory
+
+#Get the user accounts to create
+$filelocation = \\server\share\newusers.csv
