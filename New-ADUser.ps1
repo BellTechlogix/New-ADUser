c8ce3a014b6b58c80b87f7cb51fbac28ec80b27c
Kristopher Roy <kristopher.roy@gmail.com>
2019-12-23 10:31:10 -0800

    added new user switches

--- New-ADUser.ps1
+++ New-ADUser.ps1
@@ -11,4 +11,20 @@
 Import-Module activedirectory
 
 #Get the user accounts to create
-$filelocation = \\server\share\newusers.csv
+$filelocation = "\\server\share\"
+$filename = "newusers.csv"
+$users = import-csv $filelocation$filename
+
+#loop through and create the user accounts
+FOREACH($user in $users)
+{
+	$DN = $user.FirstName+" "+$user.LastName
+	$FN = $user.FirstName
+	$LN = $user.LastName
+	$Description = $user.Description
+	$PW = $user.password
+	$OU = $user.OU
+	$SAM = $users.SAMAccountName
+	$UPN = 
+	new-aduser -server $dc -Name $DN -GivenName $FN -Surname $LN -Description $Description -Path $OU -AccountPassword (ConvertTo-SecureString $PW -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true -PasswordNeverExpires $False
+}
